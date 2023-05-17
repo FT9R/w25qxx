04d536fe97d9343d5137f3edb42e8191c3972e00
FT9R <ft9r@mail.ru>
2023-05-17 07:58:29 +0300

    project structure changed

--- SPL/Config/Sys_Init.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYS_INIT_H
-#define SYS_INIT_H
-
-#include "stm32f4xx.h"
-
-void Sys_Init(void);
-
-#endif
--- SPL/Include/Init.h
+++
@@ -1,8 +0,0 @@
-#ifndef INIT_H
-#define INIT_H
-
-#include "main.h"
-
-void IO_Init(void);
-
-#endif
