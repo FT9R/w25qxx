f5efb4ce2b25e761d2fbaa64bce0073f82565401
FT9R <ft9r@mail.ru>
2023-05-17 08:15:13 +0300

    repo structure overhaul

---
+++ SPL/Include/Init.h
@@ -0,0 +1,8 @@
+#ifndef INIT_H
+#define INIT_H
+
+#include "main.h"
+
+void IO_Init(void);
+
+#endif
---
+++ SPL/Source/main.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+w25qxx_HandleTypeDef w25qxx_Handle;
+const uint8_t bufferWrite[] =
+	{1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+	 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+uint8_t bufferRead[sizeof(bufferWrite)];
+bool FLASH_access = false;
+
+void main(void)
+{
+	asm("CPSID i");
+	Sys_Init();
+	IO_Init();
+	SPIx_Init(SPI1, SPI_Mode_Master, SPI_BaudRatePrescaler_256);
+	w25qxx_Init(&w25qxx_Handle, SPI1, CS0_GPIO_Port, CS0_Pin);
+	memset(bufferRead, NULL, sizeof(bufferRead));
+	asm("CPSIE i");
+
+	if (w25qxx_Handle.status == SUCCESS)
+	{
+		// w25qxx_Erase(&w25qxx_Handle, chipErase, NULL, busyWait);
+		printf("\r\n First approach to read \r\n");
+		w25qxx_Read(&w25qxx_Handle, bufferRead, sizeof(bufferRead), PAGE_ADDRESS);
+		if (strncmp((const char *)bufferRead, (const char *)bufferWrite, sizeof(bufferRead)) == 0)
+		{
+			printf("Data already exist at page %i boundaries \r\n", PAGE);
+		}
+		else
+		{
+			printf("Data doesn't exist at page %i boundaries \r\n", PAGE);
+			printf("Page programming...");
+			w25qxx_Write(&w25qxx_Handle, bufferWrite, sizeof(bufferWrite), PAGE_ADDRESS, delayWait);
+			printf("\r\n Second approach to read \r\n");
+			w25qxx_Read(&w25qxx_Handle, bufferRead, sizeof(bufferRead), PAGE_ADDRESS);
+			if (strncmp((const char *)bufferRead, (const char *)bufferWrite, sizeof(bufferRead)) == 0)
+			{
+				printf("Writing process success \r\n");
+			}
+			else
+				printf("Writing process failure \r\n");
+		}
+	}
+	else
+		printf("Couldn't get any response from device \r\n");
+
+	while (true)
+	{
+	}
+}
