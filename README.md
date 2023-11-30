f1be4166c1c5efaaf6590334b87dbdbc8d55f846
FT9R <ft9r@mail.ru>
2023-11-30 16:17:04 +0300

    cross-platform branch

---
+++ README.md
@@ -0,0 +1,72 @@
+# Description
+A simple library designed to perform basic operations with serial flash memory devices of the w25qxx family.
+## Notes
+1. You should erase target page before data write (minimal erase operation is 1 sector or 16 pages)
+2. To make the use of the library as safe and understandable as possible, any operations with data are performed only starting from the first byte of the page 
+(e.g., for the first page the address should be 0, for the second page - 256, etc.).
+## Features
+* Many devices on the same bus are supported with its dedicated handles:
+```C
+/* First device */
+w25qxx_Init(&w25qxx_Handle, &hspi1, CS0_GPIO_Port, CS0_Pin);
+
+/* Second device */
+w25qxx_Init(&w25qxx_Handle1, &hspi1, CS1_GPIO_Port, CS1_Pin);
+
+/* Third device */
+w25qxx_Init(&w25qxx_Handle2, &hspi1, CS2_GPIO_Port, CS2_Pin);
+```
+* Data transfer is carried out by standard SPI instructions, using the CLK, /CS, DI, DO pins.  
+* Based on the device ID this library can calculate the number of pages to eliminate some address issues for write/read and erase operations.
+* There are several options for waiting for the end of page program/erase instruction with dedicated timeouts.
+* The built-in ModBus CRC can be used to ensure data integrity.
+* Fast read option is implemented in case if SPIclk > 50MHz.
+* Device status can be controlled within its handle. 
+## Supported devices
+* w25q80
+* w25q16
+* w25q32
+* w25q64
+* w25q128
+
+# Quick start
+## Common routine
+* Don't forget the following line:
+```C
+#include "w25qxx.h"
+```
+* Declare the device handle:
+```C
+w25qxx_HandleTypeDef w25qxx_Handle;
+```
+* Provide defines regarding to Chip Select pin:
+```C
+#define CS0_Pin       GPIO_PIN_15
+#define CS0_GPIO_Port GPIOA
+```
+* Initialize SPIx periphery
+* Initialize the FLASH device:
+```C
+w25qxx_Init(&w25qxx_Handle, &hspi1, CS0_GPIO_Port, CS0_Pin);
+```
+## Interfacing with HAL
+This library should work out of box together with HAL 
+## Interfacing with SPL
+* In `w25qxx_Interface.h` provide your own `SPI.h` and `Delay.h` includes   
+* In `w25qxx_Interface.c` change next func calls to yours:
+```C
+SPI_Transmit(hspix, pData, size, timeout);
+///
+SPI_Receive(hspix, pData, size, timeout);
+///
+Delay(ms);
+```
+Or just use existing SPL SPI driver, which are oversimplificated but still in manner of HAL driver
+
+# Example
+## Conditions
+`Toolchain: IAR EWARM v9.40.1`  
+`Target MCU: STM32F407VGT6 (STM32F4XX_M devboard)`  
+`Debugger: ST-LINK/V2 or DAPLink`
+## References
+For application use refer to [`HAL/../main.c`](./HAL/Core/Src/main.c) or [`SPL/../main.c`](./SPL/Source/main.c) 
---
+++ w25qxx/w25qxx.h
@@ -0,0 +1,204 @@
+#ifndef W25QXX_H
+#define W25QXX_H
+
+#include "w25qxx_Interface.h"
+
+/* Instruction Set */
+#define W25QXX_CMD_WRITE_ENABLE              0x06
+#define W25QXX_CMD_VOLATILE_SR_WRITE_ENABLE  0x50
+#define W25QXX_CMD_WRITE_DISABLE             0x04
+#define W25QXX_CMD_RELEASE_POWER_DOWN        0xAB
+#define W25QXX_CMD_MANUFACTURER_DEVICE_ID    0x90
+#define W25QXX_CMD_JEDEC_ID                  0x9F
+#define W25QXX_CMD_READ_UNIQUE_ID            0x4B
+#define W25QXX_CMD_READ_DATA                 0x03
+#define W25QXX_CMD_FAST_READ                 0x0B
+#define W25QXX_CMD_PAGE_PROGRAM              0x02
+#define W25QXX_CMD_SECTOR_ERASE_4KB          0x20
+#define W25QXX_CMD_BLOCK_ERASE_32KB          0x52
+#define W25QXX_CMD_BLOCK_ERASE_64KB          0xD8
+#define W25QXX_CMD_CHIP_ERASE                0xC7
+#define W25QXX_CMD_READ_STATUS_REGISTER1     0x05
+#define W25QXX_CMD_WRITE_STATUS_REGISTER1    0x01
+#define W25QXX_CMD_READ_STATUS_REGISTER2     0x35
+#define W25QXX_CMD_WRITE_STATUS_REGISTER2    0x31
+#define W25QXX_CMD_READ_STATUS_REGISTER3     0x15
+#define W25QXX_CMD_WRITE_STATUS_REGISTER3    0x11
+#define W25QXX_CMD_READ_SFDP_REGISTER        0x5A
+#define W25QXX_CMD_ERASE_SECURITY_REGISTER   0x44
+#define W25QXX_CMD_PROGRAM_SECURITY_REGISTER 0x42
+#define W25QXX_CMD_READ_SECURITY_REGISTER    0x48
+#define W25QXX_CMD_GLOBAL_BLOCK_LOCK         0x7E
+#define W25QXX_CMD_GLOBAL_BLOCK_UNLOCK       0x98
+#define W25QXX_CMD_READ_BLOCK_LOCK           0x3D
+#define W25QXX_CMD_INDIVIDUAL_BLOCK_LOCK     0x36
+#define W25QXX_CMD_INDIVIDUAL_BLOCK_UNLOCK   0x39
+#define W25QXX_CMD_ERASE_PROGRAM_SUSPEND     0x75
+#define W25QXX_CMD_ERASE_PROGRAM_RESUME      0x7A
+#define W25QXX_CMD_POWER_DOWN                0xB9
+#define W25QXX_CMD_ENABLE_RESET              0x66
+#define W25QXX_CMD_RESET_DEVICE              0x99
+
+/* Timings [ms] */
+#define W25QXX_PAGE_PROGRAM_TIME          3
+#define W25QXX_WRITE_STATUS_REGISTER_TIME 15
+#define W25QXX_SECTOR_ERASE_TIME_4KB      400
+#define W25QXX_BLOCK_ERASE_TIME_32KB      1600
+#define W25QXX_BLOCK_ERASE_TIME_64KB      2000
+#define W25QXX_CHIP_ERASE_TIME            25000
+
+/* Timeouts [ms] */
+#define W25QXX_TX_TIMEOUT       100
+#define W25QXX_RX_TIMEOUT       100
+#define W25QXX_RESPONSE_TIMEOUT 100
+
+/* Device constants */
+#define W25QXX_MANUFACTURER_ID 0xEF
+#define W25QXX_PAGE_SIZE       256
+#define W25QXX_SECTOR_SIZE_4KB ((W25QXX_PAGE_SIZE) * 16)
+#define W25QXX_BLOCK_SIZE_32KB ((W25QXX_PAGE_SIZE) * 128)
+#define W25QXX_BLOCK_SIZE_64KB ((W25QXX_PAGE_SIZE) * 256)
+
+enum w25qxx_Device_e { W25Q80 = 0x13, W25Q16, W25Q32, W25Q64, W25Q128 };
+
+/* Macro */
+#define KB_TO_BYTE(KB) ((KB) * 1024)
+#define ADDRESS_BYTES_SWAP(DEVICE_HANDLE, ADDRESS)                  \
+    (DEVICE_HANDLE)->addressBytes[0] = (uint8_t) ((ADDRESS) >> 16); \
+    (DEVICE_HANDLE)->addressBytes[1] = (uint8_t) ((ADDRESS) >> 8);  \
+    (DEVICE_HANDLE)->addressBytes[2] = (uint8_t) ((ADDRESS) >> 0)
+
+/* Data types */
+typedef enum w25qxx_EraseInstruction_e {
+    W25QXX_SECTOR_ERASE_4KB,
+    W25QXX_BLOCK_ERASE_32KB,
+    W25QXX_BLOCK_ERASE_64KB,
+    W25QXX_CHIP_ERASE
+} w25qxx_EraseInstruction_t;
+
+typedef enum w25qxx_WaitForTask_e { W25QXX_WAIT_NO, W25QXX_WAIT_DELAY, W25QXX_WAIT_BUSY } w25qxx_WaitForTask_t;
+
+typedef enum w25qxx_Status_e {
+    W25QXX_STATUS_NOLINK,
+    W25QXX_STATUS_RESET,
+    W25QXX_STATUS_READY,
+    W25QXX_STATUS_BUSY_INIT,
+    W25QXX_STATUS_BUSY_WRITE,
+    W25QXX_STATUS_BUSY_READ,
+    W25QXX_STATUS_BUSY_ERASE,
+} w25qxx_Status_t;
+
+typedef enum w25qxx_Error_e {
+    W25QXX_ERROR_NONE,
+    W25QXX_ERROR_REFERENCE_HANDLE,
+    W25QXX_ERROR_STATUS_MATCH,
+    W25QXX_ERROR_INITIALIZATION,
+    W25QXX_ERROR_ARGUMENT,
+    W25QXX_ERROR_ADDRESS,
+    W25QXX_ERROR_SPI,
+    W25QXX_ERROR_TIMEOUT,
+    W25QXX_ERROR_MEM_MANAGE,
+    W25QXX_ERROR_CHECKSUM,
+    W25QXX_ERROR_INSTRUCTION
+} w25qxx_Error_t;
+
+typedef struct w25qxx_HandleTypeDef_s
+{
+    uint8_t ID[2];
+    uint32_t numberOfPages;
+    uint8_t statusRegister;
+    uint8_t CMD;
+    uint8_t addressBytes[3];
+    uint16_t frameLength;
+    uint16_t CRC16;
+    w25qxx_Status_t status;
+    w25qxx_Error_t error;
+    struct
+    {
+        ErrorStatus (*Receive)(uint8_t *pDataRx, uint16_t size, uint32_t timeout);
+        ErrorStatus (*Transmit)(uint8_t *pDataTx, uint16_t size, uint32_t timeout);
+        void (*CS_Set)(w25qxx_CS_State_t newState);
+        void (*Delay)(uint32_t ms);
+    } interface;
+} w25qxx_HandleTypeDef;
+
+/**
+ * @brief
+ * @param w25qxx_Handle
+ * @param fpReceive
+ * @param fpTransmit
+ * @param fpCS_Set
+ * @param fpDelay
+ * @return
+ */
+w25qxx_Error_t w25qxx_Link(w25qxx_HandleTypeDef *w25qxx_Handle, ErrorStatus (*fpReceive)(uint8_t *, uint16_t, uint32_t),
+                           ErrorStatus (*fpTransmit)(uint8_t *, uint16_t, uint32_t),
+                           void (*fpCS_Set)(w25qxx_CS_State_t), void (*fpDelay)(uint32_t));
+
+/**
+ * @brief Checks if the device is available and determines the number of pages
+ * @param w25qxx_Handle: pointer to the device handle structure
+ * @return Device status
+ */
+w25qxx_Error_t w25qxx_Init(w25qxx_HandleTypeDef *w25qxx_Handle);
+
+/**
+ * @brief
+ * @param w25qxx_Handle
+ */
+w25qxx_Error_t w25qxx_ResetError(w25qxx_HandleTypeDef *w25qxx_Handle);
+
+/**
+ * @brief Writes data to w25qxx from external buffer
+ * @param w25qxx_Handle: pointer to the device handle structure
+ * @param buf: pointer to external buffer, that contains the data to write
+ * @param dataLength: number of bytes to write
+ * @param address: page address to write (multiple of 256 bytes)
+ * @param trailingCRC: insert or not insert CRC at the end of frame
+ * @param waitForTask: the way to ensure that operation is completed
+ * @return Device status
+ */
+w25qxx_Error_t w25qxx_Write(w25qxx_HandleTypeDef *w25qxx_Handle, const uint8_t *buf, uint16_t dataLength,
+                            uint32_t address, bool trailingCRC, w25qxx_WaitForTask_t waitForTask);
+
+/**
+ * @brief Reads data from w25qxx to external buffer
+ * @param w25qxx_Handle: pointer to the device handle structure
+ * @param buf: pointer to external buffer, that will contain the received data
+ * @param dataLength: number of bytes to read
+ * @param address: page address to read (multiple of 256 bytes)
+ * @param trailingCRC: compare or not compare CRC at the end of frame
+ * @param fastRead: set true if SPIclk > 50MHz
+ * @return Device status
+ */
+w25qxx_Error_t w25qxx_Read(w25qxx_HandleTypeDef *w25qxx_Handle, uint8_t *buf, uint16_t dataLength, uint32_t address,
+                           bool trailingCRC, bool fastRead);
+
+/**
+ * @brief Begins erase operation of sector, block or whole memory array
+ * @param w25qxx_Handle: pointer to the device handle structure
+ * @param eraseInstruction: pages groups to be erased
+ * @param address: start address of page, block or sector to be erased
+ * @param waitForTask: the way to ensure that operation is completed
+ * @return Device status
+ * @note Address has to be 0 in case of chip erase
+ */
+w25qxx_Error_t w25qxx_Erase(w25qxx_HandleTypeDef *w25qxx_Handle, w25qxx_EraseInstruction_t eraseInstruction,
+                            uint32_t address, w25qxx_WaitForTask_t waitForTask);
+
+/**
+ * @brief Checks if the device is busy or not
+ * @param w25qxx_Handle: pointer to the device handle structure
+ * @return True - device is busy
+ */
+w25qxx_Error_t w25qxx_BusyCheck(w25qxx_HandleTypeDef *w25qxx_Handle);
+
+/**
+ * @brief
+ * @param w25qxx_Handle
+ * @param timeout
+ * @return
+ */
+w25qxx_Error_t w25qxx_WaitWithTimeout(w25qxx_HandleTypeDef *w25qxx_Handle, uint32_t timeout);
+
+#endif
