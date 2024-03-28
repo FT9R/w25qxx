5cf7d608118aa0827a81377fdbe5423a5abe38fe
FT9R <ft9r@mail.ru>
2024-03-28 08:31:21 +0300

    wildcard is used now in .bat file

--- UpdateArduinoDir.bat
+++ UpdateArduinoDir.bat
@@ -1,10 +1,7 @@
 del ".\Examples\ArduinoNano\w25qxx.*"
 del ".\Examples\ArduinoNano\w25qxx_Demo.*"
 
-xcopy ".\w25qxx\w25qxx.h" ".\Examples\ArduinoNano"
-xcopy ".\w25qxx\w25qxx.c" ".\Examples\ArduinoNano"
-xcopy ".\w25qxx\w25qxx_Demo.h" ".\Examples\ArduinoNano"
-xcopy ".\w25qxx\w25qxx_Demo.c" ".\Examples\ArduinoNano"
+xcopy ".\w25qxx\w25qxx.*" ".\Examples\ArduinoNano"
+xcopy ".\w25qxx\w25qxx_Demo.*" ".\Examples\ArduinoNano"
 
-rename ".\Examples\ArduinoNano\w25qxx.c" "w25qxx.cpp"
-rename ".\Examples\ArduinoNano\w25qxx_Demo.c" "w25qxx_Demo.cpp"
+rename ".\Examples\ArduinoNano\*.c" "*.cpp"
