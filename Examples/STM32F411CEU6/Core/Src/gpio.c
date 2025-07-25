b7e693db9d7bc3ea9d9dd7b3b8592969993194a6
FT9R <ft9r@mail.ru>
2025-07-25 14:14:17 +0300

    fix misconfig CS0 pin

--- Examples/STM32F411CEU6/Core/Src/gpio.c
+++ Examples/STM32F411CEU6/Core/Src/gpio.c
@@ -65,8 +65,8 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : SPI1_CS0_Pin */
   GPIO_InitStruct.Pin = SPI1_CS0_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
   HAL_GPIO_Init(SPI1_CS0_GPIO_Port, &GPIO_InitStruct);
 
